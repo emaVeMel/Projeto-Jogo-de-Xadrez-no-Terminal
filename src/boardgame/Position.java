4984b6986f5c0cfd72d1cf04dcba6779042111b6
Emanuel Velozo <emanuel.vemel@gmail.com>
2024-03-22 13:41:59 -0300

    to string method

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -24,4 +24,9 @@ public class Position {
         this.row = row;
         this.column = column;
     }
+
+    @Override
+    public String toString() {
+        return row + ", " + column;
+    }
 }
