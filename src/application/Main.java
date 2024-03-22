e64b2051e4bca71ec1204080cdfc980937eac021
Emanuel Velozo <emanuel.vemel@gmail.com>
2024-03-22 16:52:23 -0300

    chess layer and print the board

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,10 @@
 package application;
 
+import chess.ChessMatch;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test1");
+        ChessMatch match = new ChessMatch();
+        UI.printBoard(ChessMatch.getPieces());
     }
 }
