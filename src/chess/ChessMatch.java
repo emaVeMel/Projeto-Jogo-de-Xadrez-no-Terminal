456e56432949022c7a3928125b813bc75cc3353f
Emanuel Velozo <emanuel.vemel@gmail.com>
2024-03-26 15:04:28 -0300

    Piece move count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -76,7 +76,8 @@ public class ChessMatch {
     }
 
     private static Piece makeMove(Position source, Position target) {
-        Piece p = board.removePiece(source);
+        ChessPiece p =(ChessPiece) board.removePiece(source);
+        p.increaseMoveCount();
         Piece capturedPiece = board.removePiece(target);
         if (capturedPiece != null) {
             piecesOnTheBoard.remove(capturedPiece);
@@ -87,7 +88,8 @@ public class ChessMatch {
     }
 
     private static void undoMove(Position source, Position target, Piece capturedPiece) {
-        Piece p = board.removePiece(target);
+        ChessPiece p = (ChessPiece) board.removePiece(target);
+        p.decreaseMoveCount();
         board.placePiece(p, source);
         if (capturedPiece != null) {
             board.placePiece(capturedPiece, target);
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -6,14 +6,28 @@ import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
     private Color color;
+    private Integer moveCount;
 
     public ChessPiece(Board board, Color color) {
         super(board);
         this.color = color;
+        moveCount = 0;
     }
 
     public Color getColor() {
         return color;
+    }
+
+    public Integer getMoveCount() {
+        return moveCount;
+    }
+
+    protected void increaseMoveCount() {
+        moveCount++;
+    }
+
+    protected void decreaseMoveCount() {
+        moveCount--;
     }
 
     public ChessPosition getChessPosition() {
